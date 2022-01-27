ca11922ec3efbaa0b6f36b2741ec7fcb7df5a3b7
allan kisuule <allankisuule@gmail.com>
2022-01-27 20:25:32 +0100

    updated the style

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   root to: 'movies#index'
-=======
-  resources :articles
->>>>>>> f25bf81a69c34532ccf213a4107fa8a03c937177
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :movies
 end
