d0e48ea4d377ee638ea2577964e521b75ac1f74e
allan kisuule <allankisuule@gmail.com>
2022-03-01 21:23:58 +0100

    reedited this exercise

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
-  validates :title, :overview, presence: true, uniqueness: true
-  # validates :rating, inclusion: { in: 0..10 }, numericality: { only_integer: true }
+
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
 end
--- db/migrate/20220122120322_remove_references_from_bookmarks.rb
+++
@@ -1,6 +0,0 @@
-class RemoveReferencesFromBookmarks < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :bookmarks, :movie_id, :integer
-    remove_column :bookmarks, :list_id, :integer
-  end
-end
